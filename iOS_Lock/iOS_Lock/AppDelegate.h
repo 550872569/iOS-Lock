f295ebe38d98aef1d93e84524b172ba847eeef9b
YY <550872569@qq.com>
2018-09-02 07:12:03 +0800

    init iOS-Lock

---
+++ iOS_Lock/iOS_Lock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_Lock
+//
+//  Created by Yan on 2018/9/2.
+//  Copyright © 2018年 YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
